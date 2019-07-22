fc9fa73164045525071adc2433301c2471c1084e
thomascrines <thomascrines@hotmail.com>
2019-07-22 12:38:53 +0100

    add shs_rename_columns

---
+++ R/shs_rename_columns.R
@@ -0,0 +1,16 @@
+#' Rename column names in Rda files
+#'
+#' \code{shs_rename_columns} renames column names in Rda files in extracted datasets according to data specified in extracted metadata.
+#'
+#' @return \code{null}.
+#'
+#' @examples
+#' shs_rename_columns()
+#'
+#' @export
+
+
+# Set up input and output directories
+shs_rename_columns <- function() {
+
+}
