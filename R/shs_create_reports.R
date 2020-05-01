f5ecdaa50948782c955490f34c01b46d63464ce7
thomascrines <thomascrines@hotmail.com>
2020-05-01 14:29:24 +0100

    reports working

--- R/shs_create_reports.R
+++ R/shs_create_reports.R
@@ -47,7 +47,7 @@ header-includes:
     - \\hypersetup{colorlinks=true, linkcolor = black, urlcolor = [RGB]{0, 163, 163}}
     - \\usepackage{titling}
     - \\pretitle{\\begin{center}
-      \\includegraphics[width=2in,height=2in]{../www/shs-logo.png}\\LARGE\\\\}
+      \\includegraphics[width=2in,height=2in]{../www/new_logo.png}\\LARGE\\\\}
     - \\posttitle{\\end{center}}
     - \\renewcommand{\\familydefault}{\\sfdefault}
 classoption: landscape
@@ -109,9 +109,6 @@ comparison_table_title <- if (comparison_type == \"Local Authority\") {
   }
 
 ```
-```{r, out.height = \"130pt\", fig.show='hold', fig.align='center'}
-knitr::include_graphics(c(\"../www/shs-logo.png\", \"../www/home_logo.png\"))
-```
 
 ## Acknowledgements
 The Scottish Government acknowledges and thanks the 10,530
@@ -148,17 +145,7 @@ $ \\color[RGB]{0, 163, 163} \\blacksquare $ Significantly higher $ \\color[RGB]{
 
         data_file_path <- paste0("app/data/dataset/", question_id, ".Rds")
 
-        df <- readRDS(data_file_path)
-
-        column_names <- colnames(df)
-        row_lengths <- c()
-
-        for (row in 1:nrow(df)) {
-
-          row_lengths <- c(row_lengths, Reduce("+", nchar(as.character(df[row,]))))
-        }
-
-        max_row_length <- max(row_lengths)
+        column_names <- colnames(readRDS(data_file_path))
 
         significance_column_names <- gsub("_l", "", column_names[grep("_l", column_names)])
         significance_column_names <- significance_column_names[!significance_column_names %in% c("All", "Base")]
@@ -217,7 +204,7 @@ $ \\color[RGB]{0, 163, 163} \\blacksquare $ Significantly higher $ \\color[RGB]{
                          question_id_underscore, " %>%
 kable(\"latex\", escape = FALSE, booktabs = T)")
 
-        if (length(column_names) > 10) {
+        if (length(column_names[!grepl("_l", column_names) & !grepl("_u", column_names)]) > 12) {
 
           string <- paste0(string, " %>% kable_styling(latex_options = \"scale_down\")")
           }
@@ -247,7 +234,7 @@ mutate(")
 select(!tidyselect::all_of(significance_column_names)) %>%
 kable(\"latex\", escape = FALSE, booktabs = T)")
 
-        if (length(column_names) > 10) {
+        if (length(column_names[!grepl("_l", column_names) & !grepl("_u", column_names)]) > 12) {
 
           string <- paste0(string, " %>% kable_styling(latex_options = \"scale_down\")")
         }
@@ -278,8 +265,14 @@ mutate("
         string <- paste0(substr(string, 1, nchar(string ) - 2),
                          ") %>%
         select(!tidyselect::all_of(significance_column_names)) %>%
-        kable(\"latex\", escape = FALSE, booktabs = T)
-```\n")
+        kable(\"latex\", escape = FALSE, booktabs = T)")
+
+        if (length(column_names[!grepl("_l", column_names) & !grepl("_u", column_names)]) > 12) {
+
+          string <- paste0(string, " %>% kable_styling(latex_options = \"scale_down\")")
+      }
+
+      string <- paste0(string, "\n```\n")
       }
 
       if (type == 4) {
@@ -288,13 +281,27 @@ mutate("
 
 ```{r eval=(eval_comparison_time_series == FALSE)}
 ", question_id_underscore, " %>%
-kable(\"latex\", escape = FALSE, booktabs = T)
+kable(\"latex\", escape = FALSE, booktabs = T)")
+
+        if (length(column_names[!grepl("_l", column_names) & !grepl("_u", column_names)]) > 12) {
+
+          string <- paste0(string, " %>% kable_styling(latex_options = \"scale_down\")")
+        }
+
+        string <- paste0(string, "
 ```
 ```{r eval=eval_comparison_time_series}
 main_column_names <- colnames(", question_id_underscore, ")[!grepl(\"_2\", colnames(", question_id_underscore, "))]
 
 ", question_id_underscore, " %>% select(tidyselect::all_of(main_column_names)) %>%
-kable(\"latex\", escape = FALSE, booktabs = T)
+kable(\"latex\", escape = FALSE, booktabs = T)")
+
+        if (length(column_names[!grepl("_l", column_names) & !grepl("_u", column_names)]) > 12) {
+
+          string <- paste0(string, " %>% kable_styling(latex_options = \"scale_down\")")
+        }
+
+        string <- paste0(string, "
 ```
 ```{r eval=eval_comparison_time_series}
 asis_output(paste0(\"### \", comparison_table_title))
@@ -308,7 +315,7 @@ comparison_rename_column_names <- gsub(\"_2\", \"\", comparison_column_names)
 rename_at(comparison_column_names, ~ comparison_rename_column_names) %>%
 kable(\"latex\", escape = FALSE, booktabs = T)")
 
-        if (length(column_names) > 10) {
+        if (length(column_names[!grepl("_l", column_names) & !grepl("_u", column_names)]) > 12) {
 
           string <- paste0(string, " %>% kable_styling(latex_options = \"scale_down\")")
         }
