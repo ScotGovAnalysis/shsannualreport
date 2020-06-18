049ff10590f8e6ebd5593d01db270879caf170ef
thomascrines <thomascrines@hotmail.com>
2020-06-18 15:27:34 +0100

    remove app from package

--- test_script.R
+++ test_script.R
@@ -2,14 +2,12 @@
 #
 # source_data_directory <- "C:/Users/dsap01/Documents/shsannualreportdata"
 #
-# top_level_directory <- "C:/Users/dsap01/Documents"
+# top_level_directory <- "C:/Users/dsap01/Documents/SHS Data Explorer"
 #
 # columns_to_remove <- c("sort", "_LABEL_", "var", "LABEL")
 #
 # reports_start_year <- 2013
 # reports_end_year <- 2018
-#
-# top_level_directory <- file.path(top_level_directory, "SHS Data Explorer")
 #
 # shsannualreport::shs_app_processing_1(top_level_directory = top_level_directory,
 #                                       columns_to_remove = columns_to_remove)
